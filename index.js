6880b13a929b604f35018a8b5f8169e02f0c0836
clan9 <sglennie289@gmail.com>
2019-09-20 09:34:50 +0100

    setup keys for hosted app

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ app.use('/api/auth', authRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/user', userRouter);
 
+// setup static files for hosting
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like main.js or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up index.html file
+  // if it doesn't recognise the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
